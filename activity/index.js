a68e6c11f70448c8bb0f7b2066ee1353a2d7a428
ssskram <ssskram@ssskram.local>
2019-03-08 09:59:03 -0500

    first

---
+++ activity/index.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch')
+global.Headers = fetch.Headers
+
+module.exports = async (context, timer) => {
+
+        const now = new Date()
+}
---
+++ alerts/index.js
@@ -0,0 +1,6 @@
+const fetch = require('node-fetch')
+global.Headers = fetch.Headers
+
+module.exports = async (context, req) => {
+
+}
